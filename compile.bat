6b0dde7e160e3e7bdbe83a6a73f6fbc12211dec5
elouannh <elouannhosta01@gmail.com>
2024-01-29 23:28:42 +0100

    Updated the batch script. Added the Python file.

--- compile.bat
+++ compile.bat
@@ -1,9 +1,11 @@
 @echo off
 
-set EXECUTABLE=program.exe
-
+set PYTHONSOURCE=setup/csv_generator.py
 set SOURCES=main.c src/csv_manager.c
 
+set EXECUTABLE=program.exe
+
+python %PYTHONSOURCE%
 gcc -mconsole %SOURCES% -o %EXECUTABLE%
 
 if %errorlevel% equ 0 (
