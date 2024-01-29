de10fc6137617093ef998d1376bae004c64b44f3
elouannh <elouannhosta01@gmail.com>
2024-01-29 23:29:59 +0100

    Updated the batch script. Added the Python file.

--- compile.bat
+++ compile.bat
@@ -12,6 +12,8 @@ if %errorlevel% equ 0 (
     echo Compilation succeeded. The executable is named %EXECUTABLE%.
 
     echo Running the program...
+    for /L %%i in (1,1,3) do (echo .)
+
     .\%EXECUTABLE%
 ) else (
     echo Compilation failed.
