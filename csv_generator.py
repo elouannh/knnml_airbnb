039f63ba604ea2c3841703b3637cf23f53374b5f
elouannh <elouannhosta01@gmail.com>
2024-01-29 13:15:04 +0100

    Initial commit

---
+++ csv_generator.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+29/01/2024
+@author: Elouann HOSTA
+"""
+import random
+
+columns = {
+    "id": ("int", "index"),
+    "price": ("float", 20, 100),
+    "rooms": ("int", 1, 4),
+    "accommodates": ("int", 1, 8),
+    "cleaning_fee": ("float", 40, 200),
+    "latitude": ("float", -90, 90),
+    "longitude": ("float", -180, 180)
+}
+
+
+
