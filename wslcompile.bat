112cfc73cb0565a2191923a26d1b3d3f0fc2848a
elouannh <elouannhosta01@gmail.com>
2024-01-30 00:02:28 +0100

    Added a script for the WSL

---
+++ wslcompile.bat
@@ -0,0 +1,28 @@
+@echo off
+
+set PYTHONSOURCE=setup/csv_generator.py
+set SOURCES=main.c src/csv_manager.c
+
+set EXECUTABLE=program.exe
+
+for /f %%i in ("%~dp0") do set "CURDIR=%%~fi"
+
+set "WSLPATH=%CURDIR:\=/%"
+
+set "WSLPATH=%WSLPATH:~0,-1%"
+
+set "WSLPATH=/mnt/c%WSLPATH:~2%"
+
+start wsl python3 %WSLPATH%/%PYTHONSOURCE%
+start wsl gcc %WSLPATH%/%SOURCES% -o %WSLPATH%/%EXECUTABLE%
+
+if %errorlevel% equ 0 (
+    echo Compilation succeeded. The executable is named %EXECUTABLE%.
+
+    echo Running the program...
+    for /L %%i in (1,1,3) do (echo .)
+
+    start wsl %WSLPATH%/%EXECUTABLE%
+) else (
+    echo Compilation failed.
+)
